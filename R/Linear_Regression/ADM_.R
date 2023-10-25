32c2e0d651c6313675e5acda2e5495a85667025e
Abishek <www.abishekmannai2004@gmail.com>
2023-10-25 15:45:53 +0530

    Add files via upload

--- R/Linear_Regression/ADM_.R
+++ R/Linear_Regression/ADM_.R
@@ -1,2 +1,27 @@
-adm <- read.csv("Admission_Predict.csv")
-View(adm)
+#--------------------------------------------<IMP-DATASE>---------------------------------------------#
+server <- read.csv("Admission_Predict.csv")
+View(server)
+str(server)
+
+#--------------------------------------------<SLICING-DATA>---------------------------------------------#
+gre = server [,2]
+coa = server [,9]
+
+#--------------------------------------------<Dataset-For-SLR>---------------------------------------------#
+data =data.frame(gre, coa)
+View(data)
+
+#--------------------------------------------<IMPORTING-DATA-SPLITING-LIBRARY>---------------------------------------------#
+library(caTools)
+
+#--------------------------------------------<GEM-RANDOM-NUM>---------------------------------------------#
+set.seed(42)
+
+#--------------------------------------------<DATA-SPLITING>---------------------------------------------#
+split = sample.split(data$coa, SplitRatio = 0.8)
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+training_set = subset(data, split==TRUE)
+View(training_set)
+training_set = subset(data, split==FALSE)
+View(training_set)
