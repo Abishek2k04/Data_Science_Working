677659e17d638bd6a199a345d4e459f6bb9d997e
Abishek <www.abishekmannai2004@gmail.com>
2023-10-25 15:47:25 +0530

    Update ADM_.R

--- R/Linear_Regression/ADM_.R
+++ R/Linear_Regression/ADM_.R
@@ -23,5 +23,27 @@ split = sample.split(data$coa, SplitRatio = 0.8)
 #--------------------------------------------<Training-Data>---------------------------------------------#
 training_set = subset(data, split==TRUE)
 View(training_set)
-training_set = subset(data, split==FALSE)
+testing_set = subset(data, split==FALSE)
 View(training_set)
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+regressor = lm(formula = coa~gre, data = training_set)
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+y_pred = predict(regressor, newdata = testing_set)
+View(y_pred)
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+library(ggplot2)
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+plot(testing_set$gre, testing_set$coa, type= 'p',
+     col ='blue', xlab = 'GRE', ylab = 'COA')
+
+lines(testing_set$gre, y_pred ,type= 'o', col='red')
+
+#--------------------------------------------<Training-Data>---------------------------------------------#
+ggplot(testing_set, aes(x = gre)) +
+   geom_line(aes(y=coa, color = 'Actual')) +
+   geom_line(aes(y=y_pred, color = 'Predicted'))
+ 
