af5ea299396ee1afca38a7499129ed92ffef741c
Abishek <www.abishekmannai2004@gmail.com>
2024-03-30 11:20:40 +0530

    Create TextManupulation.R

---
+++ IV Sem/TextManupulation.R
@@ -0,0 +1,21 @@
+library(robotstxt)
+library(rvest)
+
+url <- "https://www.espncricinfo.com/story/anantha-narayanan-jayasuriya-stunner-in-singapore-and-all-the-other-outliers-in-odi-history-1348299"
+
+path = paths_allowed(url)
+
+web = read_html(url)
+View(web)     
+
+content <- web %>% html_nodes(".ci-html-content div") %>% html_text()
+View(content)
+#change to lowercase
+lowc = tolower(content)
+View(lowc)
+lowc
+#change to uppercase
+highc = toupper(content)
+View(lowc)
+highc
+
